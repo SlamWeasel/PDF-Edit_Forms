76721978a306e78ec58c8c4f95aa58c32d400f04
SlamWeasel <63350349+SlamWeasel@users.noreply.github.com>
2022-01-04 15:19:03 +0100

    Renaming

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
-namespace PDF_Edit_Froms
+namespace PDF_Edit_Forms
 {
     static class Program
     {
@@ -13,7 +13,7 @@ namespace PDF_Edit_Froms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
