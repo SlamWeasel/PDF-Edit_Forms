9a84dbd17d39bb4393078384d6549b97cda7e2a9
SlamWeasel <63350349+SlamWeasel@users.noreply.github.com>
2022-01-04 11:46:13 +0100

    Renaming

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("PDF-Edit_Froms")]
+[assembly: AssemblyTitle("PDF-Edit_Forms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PDF-Edit_Froms")]
+[assembly: AssemblyProduct("PDF-Edit_Forms")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
